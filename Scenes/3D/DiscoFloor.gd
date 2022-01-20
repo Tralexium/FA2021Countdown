bff53c13135157ab2ad37baa449e00421ff351ce
Tralexium <alextheloler@gmail.com>
2022-01-20 02:12:11 +0000

    Initial verion of the Main scene.

--- Scenes/3D/DiscoFloor.gd
+++ Scenes/3D/DiscoFloor.gd
@@ -1,20 +1,17 @@
 extends MultiMeshInstance
 
 export(int) var disco_floor_size = 40
+export(float, 0, 1) var intensity = 0
 export(float) var tile_margin = 6.0
 # From DiscoTile Scene
 export(Gradient) var color_gradient = preload("res://Resources/disco_tiles_default_gradient.tres")
-export(float) var cycle_duration = 3.0
-export(float) var max_cycle_offset = 0.5
-export(float) var jump_dist = 2.0
-export(float) var jump_dur = 2.0
+export(float) var max_cycle_offset = 0.2
 
-onready var nCycleDuration : Timer = $CycleDuration
 var tile_alphas : Array
+var timer : float
 
 
 func _ready() -> void:
-	nCycleDuration.start(cycle_duration)
 	multimesh.instance_count = disco_floor_size*disco_floor_size
 	var i := 0
 	for x in range(disco_floor_size/-2, disco_floor_size/2):
@@ -33,11 +30,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	for i in multimesh.instance_count:
 		seed(i)
-		var _intensity : float = nCycleDuration.time_left / nCycleDuration.wait_time
 		var _rand_cycle_offset : float = randf() * max_cycle_offset
 		
-		var _offset : float = fmod((nCycleDuration.time_left + _rand_cycle_offset), cycle_duration) / cycle_duration
+		timer += delta*0.0001
+		var _offset : float = fmod((timer + _rand_cycle_offset), 1.0)
 		var _final_col : Color = color_gradient.interpolate(_offset)
 		_final_col.a = tile_alphas[i]
 		multimesh.set_instance_color(i, _final_col)
-		multimesh.set_instance_custom_data(i, Color(float(i % disco_floor_size), float(floor(i / disco_floor_size)), float(disco_floor_size), _intensity))  # pass the intensity
+		multimesh.set_instance_custom_data(i, Color(float(i % disco_floor_size), float(floor(i / disco_floor_size)), float(disco_floor_size), intensity))  # pass the intensity
---
+++ Scenes/Main.gd
@@ -0,0 +1,27 @@
+extends Node
+
+export(float) var min_db = 50.0
+export(float) var min_freq = 0.0
+export(float) var max_freq = 40.0
+export(float) var shrink_spd = 0.4
+
+onready var nDiscoFloor := $DiscoFloor
+var spectrum : AudioEffectInstance
+var energy : float
+var prev_energy : float
+
+
+func _ready() -> void:
+	spectrum = AudioServer.get_bus_effect_instance(0,0)
+	$Music.seek(30)
+
+
+func _process(delta: float) -> void:
+	var magnitude : float = spectrum.get_magnitude_for_frequency_range(min_freq, max_freq).length()
+	energy = clamp((min_db + linear2db(magnitude)) / min_db, 0, 1)
+	if energy < prev_energy:
+		energy = prev_energy - (shrink_spd*delta)
+	prev_energy = energy
+	
+	nDiscoFloor.intensity = energy
+	nDiscoFloor.rotate_y(0.1*delta)
