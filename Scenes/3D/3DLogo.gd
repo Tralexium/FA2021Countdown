84ac176938617b016e4dcdfc8c5aaa7a801c5e87
Tralexium <alextheloler@gmail.com>
2022-02-01 20:27:04 +0000

    3D Logo can now expand its segments

--- Scenes/3D/3DLogo.gd
+++ Scenes/3D/3DLogo.gd
@@ -1,10 +1,28 @@
-#tool
 extends Spatial
 
-export(float, 0, 1) var expand_perc : float setget set_expand_perc
-export(float) var expand_dist : float
+export(float, 0, 1) var expand_perc := 0.5
+export(float) var expand_dist := 1
+
+var old_expand_perc := 0.0
+var original_chunk_positions : Array
+var timer := 0.0
 
 
+func _ready() -> void:
+	for node in $FruitChunks.get_children():
+		original_chunk_positions.append(node.translation)
 
-func set_expand_perc(new_value : float) -> void:
-	expand_perc = new_value
+
+func _process(delta: float) -> void:
+	timer += delta
+	expand_perc = sin(timer) + 1
+	
+	if old_expand_perc != expand_perc:
+		old_expand_perc = expand_perc
+		
+		var i = 0
+		for node in $FruitChunks.get_children():
+			var _og_pos = original_chunk_positions[i]
+			var _travel_dist = (Vector3.ZERO.direction_to(_og_pos).normalized() * expand_dist) * expand_perc
+			node.translation = _og_pos + _travel_dist
+			i += 1
