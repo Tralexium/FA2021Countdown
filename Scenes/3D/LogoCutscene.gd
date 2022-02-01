e6529a8fc6d8fbeb63c7d05f9ab233ce02d25cba
Tralexium <alextheloler@gmail.com>
2022-02-01 23:54:14 +0000

    Adjusted cherry animation and implemented a basic script, Adjusted opacity of the BG letters

---
+++ Scenes/3D/LogoCutscene.gd
@@ -0,0 +1,24 @@
+extends Spatial
+
+export(float) var initial_rot_spd := -6.0
+export(float) var final_rot_spd := -0.5
+
+onready var rot_spd := initial_rot_spd
+
+onready var nFruitChunks := $"3DLogo/FruitChunks"
+onready var nAnim := $AnimationPlayer
+onready var nTween := $Tween
+
+
+func _ready() -> void:
+	nTween.interpolate_property(self, "rot_spd", initial_rot_spd, final_rot_spd, 4.0, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	nTween.start()
+	nAnim.play("intro")
+
+
+func _process(delta: float) -> void:
+	nFruitChunks.rotation.y += rot_spd * delta
+
+
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	pass # Replace with function body.
